a57fc3fe62bb26a948f5bde1f2eae847fea9171c
Eray Sahin <eraysahinmoon@gmail.com>
2022-10-15 23:42:40 -0400

    Update Population.h

--- PopulationProgramCpp/Population.h
+++ PopulationProgramCpp/Population.h
@@ -1,10 +1,4 @@
-/*
-Student Name: Eray Sahin
-Student ID: 156387201
-Section: NRA
-Date: 09-25-2022
-I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments.
-*/
+
 #ifndef SDDS_POPULATION_H_
 #define SDDS_POPULATION_H_
 namespace sdds {
