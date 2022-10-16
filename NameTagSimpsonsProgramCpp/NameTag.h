16d58c243218234c86c4cb67821914e00e03d35d
Eray Sahin <eraysahinmoon@gmail.com>
2022-10-15 23:41:35 -0400

    Update NameTag.h

--- NameTagSimpsonsProgramCpp/NameTag.h
+++ NameTagSimpsonsProgramCpp/NameTag.h
@@ -1,22 +1,10 @@
 #ifndef SDDS_NAMETAG_H
 #define SDDS_NAMETAG_H
-
 namespace sdds {
-
-
 	class NameTag {
 	public:	
 		char* name[51];
 		void set(const char* name);
 	};
-
-
-
-
-
-
-
 }
-
-
 #endif
