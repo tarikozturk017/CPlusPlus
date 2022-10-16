85e5cf5ff8354cd4cbac289e546dcef939b115e6
Eray Sahin <eraysahinmoon@gmail.com>
2022-10-15 23:39:54 -0400

    Update word.h

--- DictionaryAppCpp/word.h
+++ DictionaryAppCpp/word.h
@@ -1,9 +1,4 @@
-/*Student Name : Eray Sahin
-Student ID: 156387201
-Student E-mail Address: esahin2@myseneca.ca
-Date: 09-18-2022
-I have done all the coding by myself and only copied the
-code that my professor provided to complete my workshops and assignments.*/
+
 #ifndef SDDS_WORDS_H 
 #define SDDS_WORDS_H 
 namespace sdds
