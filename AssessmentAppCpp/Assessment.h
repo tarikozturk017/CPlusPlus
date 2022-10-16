da176e515221f3dc013e98e68d7a0d3c03a854f9
Eray Sahin <eraysahinmoon@gmail.com>
2022-10-15 23:36:56 -0400

    Update Assessment.h

--- AssessmentAppCpp/Assessment.h
+++ AssessmentAppCpp/Assessment.h
@@ -1,10 +1,4 @@
-/*
-Student Name: Eray Sahin
-Student ID: 156387201
-Section: NRA
-Date: 09-23-2022
-I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments.
-*/
+
 
 #ifndef _SDDS_ASSESSMENT_H
 #define _SSDS_ASSESSMENT_H
